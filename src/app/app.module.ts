115f9ffab38faa23c41ab36f3c3a311dc09791e1
Ahmed Mostafa <ahmed.mostafa8582@gmail.com>
2023-09-14 15:27:53 +0300

    Signed-off-by: Ahmed Mostafa <ahmed.mostafa8582@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvsComponent } from './tvs/tvs.component';
+import { FormsModule } from '@angular/forms';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MoviesComponent,
+    TvsComponent,
+    ItemDetailsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
